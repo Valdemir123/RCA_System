a1c2fbff2a081f6fdf14e60c41500c054b4d7ae5
Valdemir123 <vpsilva@gmail.com>
2020-06-15 17:15:37 -0300

    Mudança no menu e layout

--- RCA/Controllers/HomeController.cs
+++ RCA/Controllers/HomeController.cs
@@ -1,53 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using RCA.Models.ViewModels;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace RCA.Controllers
 {
     public class HomeController : Controller
     {
-        
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-
-        // POST: Error Message
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error(string _Message)
-        {
-            var _vm = new ErrorViewModel
-            {
-                Message = _Message,
-                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-            };
-
-            return View(_vm);
         }
     }
 }
